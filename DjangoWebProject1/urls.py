eba3d0be7bb6b2f1f4ac23239c5ca9390c7fbc5b
Shecter67 <dimars1404@yandex.ru>
2022-08-08 23:02:38 +0300

    CreateFirstPage

--- DjangoWebProject1/urls.py
+++ DjangoWebProject1/urls.py
@@ -7,24 +7,11 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf.urls import include, url
+import testDjango1.views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
+    url(r'^$', testDjango1.views.index, name='index'),
+    url(r'^home$', testDjango1.views.index, name='home'),
 ]
---
+++ testDjango1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class testDjango1Config(AppConfig):
+    name = 'testDjango1'
